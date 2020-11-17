be64e7c4986904075d9faba187d5fe76a2f6a71c
Ankandey89 <ankandelta@gmail.com>
2020-11-17 16:31:44 +0530

    added platform

--- sketch.js
+++ sketch.js
@@ -6,17 +6,20 @@ var engine, world;
 var box1, pig1;
 var backgroundImg;
 
+var platform;
+
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
 
 function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
+        var canvas = createCanvas(1200,400);
+        engine = Engine.create();
     world = engine.world;
 
     
     ground = new Ground(600,height,1200,20)
+    platform = new Ground(150,300,300,170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -59,4 +62,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
